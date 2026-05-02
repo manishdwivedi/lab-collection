ec3bb2176c96480ab8142ad28620556d7f2f9e58
manishdwivedi <manishkrdwivedi2011@gmail.com>
2026-05-02 19:30:21 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || ''}/api`, // relative URL
+  baseURL: '/api',  // ← same domain, no CORS ever again
   withCredentials: true,
 });
 
