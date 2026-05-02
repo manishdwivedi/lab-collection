ea9d1634668cefdc427f68ee24d756036b30f6b8
manishdwivedi <manishkrdwivedi2011@gmail.com>
2026-05-02 14:00:39 +0530

    Update HomePage.js

--- frontend/src/pages/patient/HomePage.js
+++ frontend/src/pages/patient/HomePage.js
@@ -24,6 +24,7 @@ export default function HomePage() {
   const { addToCart, isInCart } = useCart();
 
   useEffect(() => {
+    console.log(getCategories());
     getCategories().then(r => setCategories(r.data.categories.slice(0, 6)));
     getTests().then(r => setPopularTests(r.data.tests.slice(0, 6)));
   }, []);
